0af99e93a2b329134a2e92c19f81cf0ef2113b7f
Alexander Lapin <lapinuelle@gmail.com>
2025-08-22 00:45:58 +0300

    On the way to implement users migration

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
-	"strings"
+	"slices"
+
+	"github.com/joho/godotenv"
 )
 
 type Space struct {
@@ -16,76 +20,179 @@ type Space struct {
 	BoardId         uint   `json:"board_id"`
 }
 
-func readJsonFile(filePath string) (interface{}, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open file: %w", err)
-	}
-	defer file.Close()
-
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file: %w", err)
-	}
-
-	var data interface{}
-	if err := json.Unmarshal(bytes, &data); err != nil {
-		return nil, fmt.Errorf("failed to parse JSON: %w", err)
-	}
-
-	return data, nil
+type PlankaUser struct {
+	Username            string `json:"username"`
+	Name                string `json:"name"`
+	Email               string `json:"email"`
+	Password            string `json:"password"`
+	Phone               string `json:"phone"`
+	Organization        string `json:"organization"`
+	Language            string `json:"language"`
+	SubscribeToOwnCards bool   `json:"subscribeToOwnCards"`
 }
 
-func getSpaces(data interface{}) map[string]Space {
-	spaces := make(map[string]Space)
+func get_kaiten_users() (interface{}, error) {
+	kaitenUrl, exists := os.LookupEnv("KAITEN_URL")
+	if !exists {
+		return nil, fmt.Errorf("KAITEN_URL environment variable is not set")
+	}
+	kaitenToken, exists := os.LookupEnv("KAITEN_TOKEN")
+	if !exists {
+		return nil, fmt.Errorf("KAITEN_TOKEN environment variable is not set")
+	}
+	req, err := http.NewRequest("GET", kaitenUrl+"/api/latest/company/users", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "Bearer "+kaitenToken)
+	req.Header.Add("Content-Type", "application/json")
+	client := &http.Client{}
 
-	for _, value := range data.([]interface{}) {
-		var parent_entity_uid string = ""
-		if value.(map[string]interface{})["parent_entity_uid"] != nil {
-			parent_entity_uid = value.(map[string]interface{})["parent_entity_uid"].(string)
-		}
-		spaces[value.(map[string]interface{})["uid"].(string)] = Space{
-			Id:              uint(value.(map[string]interface{})["id"].(float64)),
-			Title:           value.(map[string]interface{})["title"].(string),
-			ParentEntityUid: parent_entity_uid,
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close() // Ensure the response body is closed
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+
+	return body, err
+}
+
+func get_planka_users_emails() ([]string, error) {
+	var emails []string
+	plankaUrl, exists := os.LookupEnv("PLANKA_URL")
+	if !exists {
+		return nil, fmt.Errorf("PLANKA_URL environment variable is not set")
+	}
+	plankaToken, exists := os.LookupEnv("PLANKA_TOKEN")
+	if !exists {
+		return nil, fmt.Errorf("PLANKA_TOKEN environment variable is not set")
+	}
+	req, err := http.NewRequest("GET", plankaUrl+"/api/users", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+plankaToken)
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close() // Ensure the response body is closed
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+	var users map[string]interface{}
+	if err := json.Unmarshal(body, &users); err != nil {
+		log.Fatalf("failed to parse JSON: %w", err)
+	}
+	for _, user := range users["items"].([]interface{}) {
+		if email, ok := user.(map[string]interface{})["email"].(string); ok && email != "" {
+			emails = append(emails, email)
+		} else {
+			log.Printf("User %s does not have a valid email", user.(map[string]interface{})["name"])
 		}
 	}
-	for key, _ := range spaces {
-		if spaces[key].ParentEntityUid != "" {
-			tempSpace := spaces[key]
-			tempSpace.Title = fmt.Sprintf("%s: %s", spaces[spaces[key].ParentEntityUid].Title, spaces[key].Title)
-			spaces[key] = tempSpace
-		}
+
+	return emails, nil
+}
+
+func create_planka_user(user PlankaUser) error {
+	plankaUrl, exists := os.LookupEnv("PLANKA_URL")
+	if !exists {
+		return fmt.Errorf("PLANKA_URL environment variable is not set")
+	}
+	plankaToken, exists := os.LookupEnv("PLANKA_TOKEN")
+	if !exists {
+		return fmt.Errorf("PLANKA_TOKEN environment variable is not set")
 	}
 
-	return spaces
+	userJson, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("error marshalling user data: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", plankaUrl+"/api/users", bytes.NewBuffer(userJson))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Authorization", "Bearer "+plankaToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	fmt.Printf("%s\n", req)
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("failed to create user, status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
 }
 
 func main() {
 
-	var boards_to_spaces = make(map[uint]uint)
+	raw_users, err := get_kaiten_users()
 
-	spacesFilePath := "/Users/xander/Downloads/export_Kaiten/exported_tables/space_part1.json"
-	spacesToBoardsPath := "/Users/xander/Downloads/export_Kaiten/exported_tables/space_boards_part1.json"
+	{
+		if err != nil {
+			log.Fatalf("Error fetching Kaiten users: %v", err)
+		}
+		var users interface{}
+		if err := json.Unmarshal(raw_users.([]byte), &users); err != nil {
+			log.Fatalf("failed to parse JSON: %w", err)
+		}
+		fmt.Printf("Users: %d\n", len(users.([]interface{})))
+		emails, err := get_planka_users_emails()
+		if err != nil {
+			log.Fatalf("Error fetching Planka user emails: %v", err)
+		}
+		fmt.Printf("Emails: %d\n", len(emails))
+		for _, user := range users.([]interface{}) {
+			if !slices.Contains(emails, user.(map[string]interface{})["email"].(string)) {
+				userData := PlankaUser{
+					Username:            user.(map[string]interface{})["username"].(string),
+					Name:                user.(map[string]interface{})["full_name"].(string),
+					Email:               user.(map[string]interface{})["email"].(string),
+					Password:            "1234tempPass", // Default password, should be changed later
+					Phone:               "1234567890",
+					Organization:        "IPPM",
+					Language:            "ru-RU",
+					SubscribeToOwnCards: false,
+				}
+				err := create_planka_user(userData)
+				if err != nil {
+					log.Printf("Error creating Planka user %s: %v", userData.Username, err)
+					continue
+				}
+				fmt.Printf("Created Planka user: %s\n", userData.Username)
 
-	data, err := readJsonFile(spacesFilePath)
-	if err != nil {
-		log.Fatalf("Error reading spaces file: %v", err)
-	}
-	var spaces = getSpaces(data)
-	data, err = readJsonFile(spacesToBoardsPath)
-	if err != nil {
-		log.Fatalf("Error reading spaces to boards file: %v", err)
-	}
-
-	for _, value := range data.([]interface{}) {
-		boards_to_spaces[uint(value.(map[string]interface{})["space_id"].(float64))] = uint(value.(map[string]interface{})["board_id"].(float64))
-	}
-	for key, _ := range spaces {
-		if !strings.HasPrefix(spaces[key].Title, "Тестирование") {
-			fmt.Printf("Space: %s, Board Id: %d\n", spaces[key].Title, boards_to_spaces[spaces[key].Id])
+				fmt.Printf("User: %s, Email: %s\n", user.(map[string]interface{})["username"].(string), user.(map[string]interface{})["email"].(string))
+			}
 		}
 	}
 
-	//fmt.Printf("Parsed JSON: %+v\n", data)
 }
